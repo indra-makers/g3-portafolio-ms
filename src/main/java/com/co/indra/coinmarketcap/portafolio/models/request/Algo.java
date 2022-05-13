e4650926d187735b58e17e562de89980147b215b
Juan Manuel Contreras Vega <64715084+JcontrerasV1997@users.noreply.github.com>
2022-05-13 14:51:26 -0500

    change success

--- src/main/java/com/co/indra/coinmarketcap/portafolio/models/request/Algo.java
+++
@@ -1,5 +0,0 @@
-package com.co.indra.coinmarketcap.portafolio.models.request;
-
-public class Algo {
-
-}
--- src/main/java/com/co/indra/coinmarketcap/portafolio/repository/AssetRepository.java
+++ src/main/java/com/co/indra/coinmarketcap/portafolio/repository/AssetRepository.java
@@ -1,5 +1,11 @@
 package com.co.indra.coinmarketcap.portafolio.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class AssetRepository {
+	
+	@Autowired
+	private JdbcTemplate template;
 
 }
