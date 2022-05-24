120b73f91c0afc0831a3e1e2bf76b31da1d8bf2b
titan9804 <jaimeandresmarinalarcon@gmail.com>
2022-05-24 18:42:51 -0500

    Update Helper.java

--- src/main/java/com/co/indra/coinmarketcap/portafolio/services/helper/Helper.java
+++ src/main/java/com/co/indra/coinmarketcap/portafolio/services/helper/Helper.java
@@ -11,35 +11,35 @@ import com.co.indra.coinmarketcap.portafolio.repository.TransactionRepository;
 
 public class Helper {
 
-   public static void helperAssetValidate(Transaction transaction, TransactionRepository transactionRepository,
-         AssetRepository assetRepository, int idAsset, List<Asset> assetList) {
-      int quantityTotal = 0;
-      Double amountTotal = 0d;
-      if (transaction.getType().equals("BUY")) {
-         transactionRepository.addTransactionToAsset(transaction, idAsset);
-         List<Transaction> transactions = transactionRepository.getTransactionByIdAsset(idAsset);
-         for (int i = 0; i < transactions.size(); i++) {
-            quantityTotal = quantityTotal + transactions.get(i).getQuantity();
-            amountTotal = amountTotal + transactions.get(i).getAmount();
-         }
-         assetRepository.updateAsset(transaction, idAsset, amountTotal, quantityTotal, assetList.get(0).getQuantity(),
-               assetList.get(0).getPrice());
-      } else if (transaction.getType().equals("SELL")) {
-         if (transaction.getQuantity() > assetList.get(0).getQuantity()) {
-            throw new BusinessException(ErrorCodes.VALUE_TO_SELL_EXCEEDS_CURRENT_VALUE);
-         } else {
-            transaction.setQuantity(-transaction.getQuantity());
+    public static void helperAssetValidate(Transaction transaction, TransactionRepository transactionRepository,
+                                           AssetRepository assetRepository, int idAsset, List<Asset> assetList) {
+        int quantityTotal = 0;
+        Double amountTotal = 0d;
+        if (transaction.getType().equals("BUY")) {
             transactionRepository.addTransactionToAsset(transaction, idAsset);
             List<Transaction> transactions = transactionRepository.getTransactionByIdAsset(idAsset);
             for (int i = 0; i < transactions.size(); i++) {
-               quantityTotal = quantityTotal + transactions.get(i).getQuantity();
-               amountTotal = amountTotal + transactions.get(i).getAmount();
+                quantityTotal = quantityTotal + transactions.get(i).getQuantity();
+                amountTotal = amountTotal + transactions.get(i).getAmount();
             }
-            assetRepository.updateAsset(transaction, idAsset, amountTotal, quantityTotal,
-                  assetList.get(0).getQuantity(), assetList.get(0).getPrice());
-         }
-      } else {
-         throw new BusinessException(ErrorCodes.UNKNOWN_TYPE);
-      }
-   }
+            assetRepository.updateAsset(transaction, idAsset, amountTotal, quantityTotal, assetList.get(0).getQuantity(),
+                    assetList.get(0).getPrice());
+        } else if (transaction.getType().equals("SELL")) {
+            if (transaction.getQuantity() > assetList.get(0).getQuantity()) {
+                throw new BusinessException(ErrorCodes.VALUE_TO_SELL_EXCEEDS_CURRENT_VALUE);
+            } else {
+                transaction.setQuantity(-transaction.getQuantity());
+                transactionRepository.addTransactionToAsset(transaction, idAsset);
+                List<Transaction> transactions = transactionRepository.getTransactionByIdAsset(idAsset);
+                for (int i = 0; i < transactions.size(); i++) {
+                    quantityTotal = quantityTotal + transactions.get(i).getQuantity();
+                    amountTotal = amountTotal + transactions.get(i).getAmount();
+                }
+                assetRepository.updateAsset(transaction, idAsset, amountTotal, quantityTotal,
+                        assetList.get(0).getQuantity(), assetList.get(0).getPrice());
+            }
+        } else {
+            throw new BusinessException(ErrorCodes.UNKNOWN_TYPE);
+        }
+    }
 }
