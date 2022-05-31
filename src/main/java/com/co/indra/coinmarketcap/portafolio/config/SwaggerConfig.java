427c63f715803d0b9c4bd49796810a07fd82a032
Juan Manuel Contreras Vega <64715084+JcontrerasV1997@users.noreply.github.com>
2022-05-31 07:02:17 -0500

    juanManuel/update

--- src/main/java/com/co/indra/coinmarketcap/portafolio/config/SwaggerConfig.java
+++ src/main/java/com/co/indra/coinmarketcap/portafolio/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.co.indra.coinmarketcap.portafolio.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -9,6 +10,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+@EnableWebMvc
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
