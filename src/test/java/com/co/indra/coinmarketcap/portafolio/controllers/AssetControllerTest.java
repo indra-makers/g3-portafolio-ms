f36a497c58aef6995d73603eb3272773bf425504
Julian Giraldo <giraldocardonajulian@gmail.com>
2022-06-17 01:31:55 -0500

    add rabbitmock

--- src/test/java/com/co/indra/coinmarketcap/portafolio/controllers/AssetControllerTest.java
+++ src/test/java/com/co/indra/coinmarketcap/portafolio/controllers/AssetControllerTest.java
@@ -8,9 +8,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.jdbc.Sql;
@@ -34,6 +36,9 @@ public class AssetControllerTest {
 
    @Autowired
    private AssetRepository assetRepository;
+
+   @MockBean
+   private RabbitTemplate rabbitTemplate;
 
    @Test
    @Sql("/testdata/addPortfolio.sql")
